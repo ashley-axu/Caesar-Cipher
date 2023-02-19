bbe60a9368935657c453984dd474e33e339f033c
Ashley XU <92137535+ashley-axu@users.noreply.github.com>
2023-02-18 22:59:14 -0800

    Update and rename proj1.java to Caesar_Cipher.java

--- Caesar_Cipher.java
+++ Caesar_Cipher.java
@@ -1,13 +1,3 @@
-/* 
-A brief summary: A Caesar cipher is a simple approach to encoding messages by shifting each letter in a message along the alphabet by a constant amount k. 
-                This project is a program that asks user to input some random constant as well as a string to be encoded. 
-                Then it returns the encoded message according to the constant user gives and decode
-                that encoded message. 
-                At the end of the program, user can enter 'y' to play again, otherwise, 'n' to quit. 
-
-Last Date Modified: 09/24/2021 
-*/ 
-
 import java.util.Arrays; 
 import java.util.Scanner; 
 
